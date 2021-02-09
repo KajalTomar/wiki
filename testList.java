a6eb67d7be646cd1c4f7cac3304038676b39e8fc
KajalTomar <kajaltomar09@gmail.com>
2021-02-09 14:30:03 -0600

    removed print statements from testList.java

--- testList.java
+++ testList.java
@@ -7,8 +7,8 @@ public class testList {
     public void testListCreation() {
         List list = new List();
         
-        System.out.println("Testing that the list was created (not null).");
-        assertNotNull(list, "should not be null.");
+        // System.out.println("Testing that the list was created (not null).");
+        assertNotNull(list);
     }
 
     @Test
@@ -18,8 +18,8 @@ public class testList {
 
         list.add(bob);
 
-        System.out.println("Testing adding an item to an empty list.");
-        assertEquals(bob, list.getFirstItem(),"user should equal the first item on the list.");
+        // System.out.println("Testing adding an item to an empty list.");
+        assertEquals(bob, list.getFirstItem());
     }
 
     @Test
@@ -27,8 +27,8 @@ public class testList {
         List list = new List();
         int total = list.getTotal();
 
-        System.out.println("Testing the getTotal method when the list is empty.");
-        assertEquals(total, 0,"getTotal should return 0 when the list is empty.");        
+        // System.out.println("Testing the getTotal method when the list is empty.");
+        assertEquals(total, 0);        
     }
 
     @Test
@@ -42,9 +42,9 @@ public class testList {
         list.add(bob);
         afterTotal = list.getTotal();
 
-        System.out.println("Testing getTotal when there is a single item on the list");
-        assertEquals(beforeTotal+1, afterTotal,"The total number of items on the list should be increase by exactly 1.");
-        assertEquals(1, afterTotal,"The total number of items on the list should be exactly 1.");
+        // System.out.println("Testing getTotal when there is a single item on the list");
+        assertEquals(beforeTotal+1, afterTotal;
+        assertEquals(1, afterTotal);
     }
 
     @Test
@@ -63,8 +63,8 @@ public class testList {
 
         total = list.getTotal();
 
-        System.out.println("Testing getTotal when there is are three items on the list");
-        assertEquals(total, 3,"The total number of items on the list should be exactly 3.");
+        // System.out.println("Testing getTotal when there is are three items on the list");
+        assertEquals(total, 3);
     }
   
     @Test
@@ -73,18 +73,18 @@ public class testList {
         User user1 = new User("user1",1);
         User user2 = new User("user2",2);
 
-        System.out.println("Testing getFirstItem (when the list is empty).");
-        assertNull(list.getFirstItem(),"first item should equal null.");
+        // System.out.println("Testing getFirstItem (when the list is empty).");
+        assertNull(list.getFirstItem());
 
         list.add(user1);
 
-        System.out.println("Testing getFirstItem (when there is only one item in the list).");
-        assertEquals(list.getFirstItem(),user1,"first item should equal user1.");
+        // System.out.println("Testing getFirstItem (when there is only one item in the list).");
+        assertEquals(list.getFirstItem(),user1);
 
         list.add(user2);
 
-        System.out.println("Testing getFirstItem.");
-        assertEquals(list.getFirstItem(),user1,"first item should still equal user1.");
+        // System.out.println("Testing getFirstItem.");
+        assertEquals(list.getFirstItem(),user1);
     }
 
     @Test
@@ -93,18 +93,18 @@ public class testList {
         User user1 = new User("user1",1);
         User user2 = new User("user2",2);
 
-        System.out.println("Testing getLastItem (when the list is empty).");
-        assertNull(list.getLastItem(),"last item should equal null.");
+        // System.out.println("Testing getLastItem (when the list is empty).");
+        assertNull(list.getLastItem());
 
         list.add(user1);
 
-        System.out.println("Testing getLastItem (when there is only one item in the list).");
-        assertEquals(list.getLastItem(),user1,"last item should equal user1.");
+        // System.out.println("Testing getLastItem (when there is only one item in the list).");
+        assertEquals(list.getLastItem(),user1);
 
         list.add(user2);
 
-        System.out.println("Testing getLastItem.");
-        assertEquals(list.getLastItem(),user2,"last item should still equal user2.");
+        // System.out.println("Testing getLastItem.");
+        assertEquals(list.getLastItem(),user2);
     }
 
     @Test
@@ -113,21 +113,21 @@ public class testList {
         User user1 = new User("user1",1);
         User user2 = new User("user2",2);
 
-        System.out.println("Testing getNextItem (when the list is empty).");
-        assertNull(list.getNextItem(user1),"next item should equal null.");
+        // System.out.println("Testing getNextItem (when the list is empty).");
+        assertNull(list.getNextItem(user1));
 
         list.add(user1);
 
-        System.out.println("Testing getNextItem (when there is only one item in the list).");
-        assertNull(list.getNextItem(user1),"next item should equal null.");
+        // System.out.println("Testing getNextItem (when there is only one item in the list).");
+        assertNull(list.getNextItem(user1));
 
         list.add(user2);
 
-        System.out.println("Testing getNextItem when passing the last item on the list.");
-        assertNull(list.getNextItem(user2),"next item should equal null.");
+        // System.out.println("Testing getNextItem when passing the last item on the list.");
+        assertNull(list.getNextItem(user2));
 
-        System.out.println("Testing getNextItem.");
-        assertEquals(list.getNextItem(user1),user2,"next item should still equal user2.");
+        // System.out.println("Testing getNextItem.");
+        assertEquals(list.getNextItem(user1),user2);
     }
 
     @Test
@@ -138,8 +138,8 @@ public class testList {
         list.add(bob);
         list.delete(bob);
 
-        System.out.println("Testing deleting an item (when there is only one item in the list).");
-        assertNull(list.getFirstItem(), "should be null.");
+        // System.out.println("Testing deleting an item (when there is only one item in the list).");
+        assertNull(list.getFirstItem());
     }
 
     @Test
@@ -152,8 +152,8 @@ public class testList {
         list.add(user2);
         list.delete(user1);
 
-        System.out.println("Testing deleting the first item on the list.");
-        assertEquals(user2, list.getFirstItem(),"user2 should equal the first item on the list.");
+        // System.out.println("Testing deleting the first item on the list.");
+        assertEquals(user2, list.getFirstItem());
 
     }
 
@@ -167,8 +167,8 @@ public class testList {
         list.add(user2);
         list.delete(user2);
 
-        System.out.println("Testing deleting the last item on the list.");
-        assertEquals(user1, list.getLastItem(),"user1 should equal the last item on the list.");
+        // // System.out.println("Testing deleting the last item on the list.");
+        assertEquals(user1, list.getLastItem());
     }
 
     @Test
@@ -185,8 +185,8 @@ public class testList {
         list.delete(notOnTheList);
         afterTotal = list.getTotal();
 
-        System.out.println("Testing deleting an item that is not on the list.");
-        assertEquals(beforeTotal, afterTotal,"The total number of items on the list should be unchanged.");
+        // // System.out.println("Testing deleting an item that is not on the list.");
+        assertEquals(beforeTotal, afterTotal);
     }
 
 }
