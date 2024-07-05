ad054f6a50a65f7742716a79d92bcaea30b89598
Kajal Tomar <52635061+KajalTomar@users.noreply.github.com>
2024-07-05 15:39:28 -0500

    Cleaning up the comments

--- Entity.java
+++ Entity.java
@@ -1,8 +1,4 @@
 //---------------------------------------------------------------------
-// CLASS: List.java
-//
-// Author: Kajal Tomar, 
-//
 // REMARKS: this is an abstract class that defines a abstract print 
 // method for it's children classes. Entity's children classes are
 // user.java, Document.java, Version.java and Line.java (all concrete).
